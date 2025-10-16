e9fc11e04bbe2be7e3598680ea0347d209bc5229
KhushbuChandel <96884266+KhushbuChandel@users.noreply.github.com>
2025-10-16 13:03:03 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+let  score = "33abc"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+/* when we know that we have to do the operation on numbers then we can use let valueInNumber = Number(score)*/
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber)
+
+
+// "33" => 33
+//"33abc" => NaN
+// true => 1; false =>0
+// undefined => NaN
+// khush => NaN
+
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1=> true; 0=>false
+// "" => false
+// "Khush" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
