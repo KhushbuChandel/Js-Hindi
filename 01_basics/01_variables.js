9c6a8b73567c48436e3ab109a1e82df33ee5796e
KhushbuChandel <96884266+KhushbuChandel@users.noreply.github.com>
2025-10-16 12:33:28 +0000

    done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -3,6 +3,7 @@ let accountEmail = "khush@google.com"
 var accountPassword = "12345"
 
 //accountId = 2  // not allowed
+let accountState;
 
 accountEmail = "kc@kc.com"
 accountPassword = "21212121"
@@ -10,4 +11,9 @@ accountCity ="Bengaluru"
 
 console.log(accountId);
 
-console.table([accountId, accountEmail,accountPassword, accountCity])
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail,accountPassword, accountCity, accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3 +3) // code readability should be high
+
+console.log("Khush")
+
+let name = "Khush"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigInt // -> used in trading,stock market or any other big website like reddit.
+
+// string => "" or '' // -> prefer to use double quote
+
+//boolean => true/false
+
+// null => standalone value  -> representation of empty value
+
+// undefined  -> we have declared a value but didn't assigned the value
+
+// symbol => unique values mostly used in react
+
+
+// Object
+
+console.log(typeof "Khush")
+
+console.log(typeof null); // output will be object
+console.log(typeof undefined); // output will be undefined
