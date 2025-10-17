291cda1daa23f5875d256fd923af799e8680cc5a
KhushbuChandel <96884266+KhushbuChandel@users.noreply.github.com>
2025-10-17 05:20:57 +0000

    datatype- summary

---
+++ datatypes-summary.js
@@ -0,0 +1,40 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id == anotherId);
+
+//const bigNumber = 23457534345433333334n
+// Non- Primitive (Reference)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"]
+let myObj={
+    name:"Khush",
+    age:22,
+}
+
+const myFunction = function(){
+   console.log("Hello world"); 
+}
+
+
+console.log(typeof bigNumber);
+console.log(typeof myFunction );
+
+console.log(typeof scoreValue );
+
+console.log(typeof userEmail );
+
+console.log(typeof outsideTemp );
