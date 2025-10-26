0efb6dd53f44bafb05d822e8912c4698d2894854
KhushbuChandel <96884266+KhushbuChandel@users.noreply.github.com>
2025-10-26 15:28:37 +0000

    04_object 2nd done

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,57 @@
+//const tinderUser = new Object()  // singleton object
+const tinderUser ={}//Non singleton object
+
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+//console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname:{
+        userfullname:{
+            firstname: "Khushbu",
+            lastname: "Chandel"
+        }
+    }
+}
+// console.log(regularUser.fullname);
+// console.log(regularUser.fullname.userfullname);
+// console.log(regularUser.fullname.userfullname.firstname);
+
+
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4:"b"}
+const obj4 = {5: "a", 6: "b"}
+
+//const obj3 = {obj1, obj2}
+//const obj3 = Object.assign(obj1, obj2)  // First method to combine the object
+//const obj3 = Object.assign({}, obj1, obj2, obj4) // 2nd method to combine objects
+
+
+const obj3 = {...obj1, ...obj2} // 3rd way to combine the objects
+//console.log(obj3);
+
+// one more syntax when we get values from database
+const users = [
+    {
+     id: 1,
+     email: "k@gmail.com"   
+    },
+     {
+     id: 1,
+     email: "k@gmail.com"   
+    }
+]
+
+users[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+// to check if your object have has that value
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
