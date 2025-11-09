3b9dc2931dff1c78bf6366fae7d464e2a1f152cb
KhushbuChandel <96884266+KhushbuChandel@users.noreply.github.com>
2025-11-09 13:16:11 +0000

    function 1 done

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,51 @@
+function sayMyName(){
+    console.log("K");
+    console.log("H");
+    console.log("U");
+    console.log("S");
+    console.log("H");
+    console.log("B");
+    console.log("U");
+
+}
+
+//sayMyName()
+
+
+// function addTwoNumbers(number1, number2){
+//     console.log(number1 + number2);
+// }
+
+// addTwoNumbers(3, 4)
+// addTwoNumbers(3, "4")
+// addTwoNumbers(3, "a")
+// addTwoNumbers(3, null)
+
+function addTwoNumbers(number1, number2){
+//     let result = number1 + number2
+//    console.log("Khushbu")
+//     return result
+    return number1 + number2
+    
+}
+
+
+const result = addTwoNumbers(3, 5)
+
+//console.log("Result:", result);
+
+
+
+function loginUserMessage(username = "Sam"){
+    //if(username === undefined)
+    if(!username){
+
+    console.log("Please enter a username");
+    return
+    }
+    return`${username} just logged in`
+}
+
+console.log(loginUserMessage("Khushbu"))
+//console.log(loginUserMessage(""))
+//console.log(loginUserMessage())
