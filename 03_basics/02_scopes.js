de07fc40fa6e408ca13b00268d2591a5af257e73
KhushbuChandel <96884266+KhushbuChandel@users.noreply.github.com>
2025-11-10 08:05:06 +0000

    scope level

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -5,8 +5,51 @@ if(true){
 let a = 10
 const b = 20
 var c = 30
-console.log("INNER: ", a);
+//console.log("INNER: ", a);
 }
-console.log(a);
+//console.log(a);
 //console.log(b);
 //console.log(c);
+
+
+function one(){
+    const username = "khushbu"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    //console.log(website);
+    two()
+}
+
+//one()
+
+if (true) {
+    const username = "khushbu"
+    if (username === "khushbu"){
+        const website = " youtube "
+        //console.log(username + website );
+    }
+    //console.log(website);
+}
+
+//console.log(username);
+
+
+
+// +++++++++++++++ example ++++++++++++++
+
+
+console.log(
+addone(5))
+function addone(num){
+    return num +1
+}
+
+
+adddTwo(5)
+const adddTwo = function(num){
+    return num +2
+}
+
