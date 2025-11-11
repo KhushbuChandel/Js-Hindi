70c1f74ab6f506c7b9193882ff1cd656c9bbbe48
KhushbuChandel <96884266+KhushbuChandel@users.noreply.github.com>
2025-11-11 12:27:33 +0000

    switch done

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,67 @@
+// switch (key) {
+//     case value:
+
+//     break;
+//     default:
+//         break;
+// }
+
+
+
+
+// const month = 3
+
+// switch (month) {
+//     case 1:
+//         console.log("January")
+        
+//         break;
+//     case 2:
+//         console.log("Feb")
+        
+//         break;
+//     case 3:
+//         console.log("March")
+        
+//         break;
+//     case 4:
+//         console.log("April")
+        
+//         break;
+    
+
+//     default:
+//         console.log("default case match");
+//         break;
+// }
+
+
+
+/// if you have another datatype like string then you use it as
+
+
+const month = "March"
+
+switch (month) {
+    case "Jan":
+        console.log("January")
+        
+        break;
+    case "Feb":
+        console.log("Feb")
+        
+        break;
+    case "March":
+        console.log("March")
+        
+        break;
+    case "April":
+        console.log("April")
+        
+        break;
+    
+
+    default:
+        console.log("default case match");
+        break;
+}
